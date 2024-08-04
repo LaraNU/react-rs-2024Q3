7e26322925655dc8f5fe0606e9c2f157e1e5b5fd
Larisa Novozhilova <kara4516@gmail.com>
2024-08-04 15:53:50 +0300

    refactor: add separate variables

--- class-components/src/store/apiSlice.ts
+++ class-components/src/store/apiSlice.ts
@@ -1,15 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const BOOKS_LIMIT = "9";
+const BOOK_LIMIT = "1";
+const BOOK_PAGE = "1";
+
 const urlSearchParamsBooks = new URLSearchParams({
   fields: "title,author_name,key,availability,cover_i",
-  limit: `${9}`,
+  limit: BOOKS_LIMIT,
 });
 
 const urlSearchParamsBook = new URLSearchParams({
   fields:
     "title,author_name,key,cover_i,first_sentence,first_publish_year,availability",
-  limit: `${1}`,
-  page: `${1}`,
+  limit: BOOK_LIMIT,
+  page: BOOK_PAGE,
 });
 
 const baseUrl = "https://openlibrary.org/search.json";
@@ -18,7 +22,7 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getBooks: builder.query({
-      query: (q: string, page: string) =>
+      query: ({ q, page }) =>
         `${baseUrl}?q=${q}&${urlSearchParamsBooks.toString()}&page=${page}`,
     }),
     getBook: builder.query({
