bb5f7678b33b23cc5503e39721246a76384cce7c
Larisa Novozhilova <kara4516@gmail.com>
2024-08-08 17:09:27 +0300

    feat: add global styles for app

--- class-components/src/pages/_app.tsx
+++ class-components/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { AppProps } from "next/app";
 import { store } from "../store/store";
+import "../global.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
