08f621de5f43ec64a174a96e465fc06b3f66bfe4
Larisa Novozhilova <kara4516@gmail.com>
2024-08-12 18:40:00 +0300

    feat: add setup for app

---
+++ react-forms/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    'prettier/prettier': 'error',
+  },
+};
